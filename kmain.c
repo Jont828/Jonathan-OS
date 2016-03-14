a7c59c3899739348fe01c805e88207ed6cbbffaf
Jont828 <Jxtdragon@gmail.com>
2016-03-13 20:57:58 -0400

    itoa() implemented

--- kmain.c
+++ kmain.c
@@ -8,14 +8,34 @@
 #define FB_HIGH_BYTE_COMMAND    14
 #define FB_LOW_BYTE_COMMAND     15
 
+//DEFINING THE 16 GLORIOUS COLORS I HAVE TO WORK WITH
 
-#define FB_GREEN     2
-#define FB_DARK_GREY 8
+#define BLACK           0
+#define BLUE            1
+#define GREEN           2
+#define CYAN            3
+#define RED             4
+#define MAGENTA         5
+#define BROWN           6
+#define LIGHT_GREY      7
+#define DARK_GREY       8
+#define LIGHT_BLUE      9
+#define LIGHT_GREEN     10
+#define LIGHT_CYAN      11
+#define LIGHT_RED       12
+#define LIGHT_MAGENTA   13
+#define LIGHT_BROWN     14
+#define WHITE           15
+
+
 
 int sum_of_three(int arg1, int arg2, int arg3);
 void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg);
 void fb_move_cursor(unsigned short pos);
-int write(char *buf);
+int write(char *buf, int pos);
+int mystrlen(char *text);
+char *itoa(int num, char *str, int base);
+void reverse(char str[]);
 
 int sum_of_three(int arg1, int arg2, int arg3)
 {
@@ -51,35 +71,103 @@ void fb_move_cursor(unsigned short pos)
     outb(FB_DATA_PORT,    pos & 0x00FF);
 }
 
-int write(char *buf)
+int write(char *buf, int pos)
 {
     if(!buf)
         return 1;
 
-    int start = 20;
-
-    // fb_move_cursor(start);
-    // fb_write_cell(start * 2, 'A', 7, 0);
+    // fb_move_cursor(pos);
+    // fb_write_cell(pos * 2, 'A', 7, 0);
     int i = 0;
     while(*(buf + i) != '\0') {
-        fb_write_cell( (start + i) * 2, buf[i], 7, 0); // changed to ( start + i ) * 2
-        fb_move_cursor(start + i);
+        fb_write_cell( (pos + i) * 2, buf[i], BLACK, LIGHT_GREY); // changed to ( pos + i ) * 2
+        fb_move_cursor(pos + i + 1); // cursor is at the space for the next character
         i++;
     }
 
     return 0;
 }
 
+int mystrlen(char *text)
+{
+    const char *c = text;
+    while(*c)
+        c++;
+
+    return c - text;
+}
+
+void reverse(char str[]) {
+    int len = mystrlen(str);
+    char c;
+    int i=0;
+    while(i < mystrlen(str) / 2) {
+        c = str[i];
+        str[i] = str[len-1];
+        str[len-1] = c;
+
+        i++;
+        len--;
+    }
+}
+
+
+
+char *itoa(int num, char *str, int base)
+{
+    int i = 0;
+    int isNegative = 0;
+
+    /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+    if (num == 0)
+    {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+ 
+    // In standard itoa(), negative numbers are handled only with 
+    // base 10. Otherwise numbers are considered unsigned.
+    if (num < 0 && base == 10)
+    {
+        isNegative = 1;
+        num = -num;
+    }
+ 
+    // Process individual digits
+    while (num != 0)
+    {
+        int rem = num % base;
+        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
+        num = num/base;
+    }
+ 
+    // If number is negative, append '-'
+    if (isNegative)
+        str[i++] = '-';
+ 
+    str[i] = '\0'; // Append string terminator
+ 
+    // Reverse the string
+    reverse(str);
+ 
+    return str;
+}
+
 
 int main(void)
 {
-	// fb_write_cell(0, 'A', 7, 0);
-    // fb_write_cell(20, 'B', 7, 0);
-	// fb_move_cursor(10);
-    // fb_move_cursor(20);
-
     char text[] = "Hello, kernel world!";
-    write(text);
+
+    write(text, 0);
+
+    int i = 25;
+    char num[20];
+    itoa(i, num, 10);
+    write(num, 30);
+    // int i;
+    // for(i=0; i<1; i++)
+    //     write(text, mystrlen(text) * i);
 
 	return 0;
 }
