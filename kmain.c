bf975149fff9cc27dad86b9194642e27908f7c75
Jont828 <Jxtdragon@gmail.com>
2016-05-10 16:43:35 -0400

    Added if else-if else branch for checking commands in an infinite loop - not sure if this is the best way so might have to change the implementation later

--- kmain.c
+++ kmain.c
@@ -21,36 +21,33 @@ void kmain(void)
     // memset(raw_buffer, '\0', 1024);
     char c;
 
-    // puts("Type something here: ");
-    // while((raw_buffer[i] = getchar()) != '0') {
-    //     if(raw_buffer[i] == '\n') {
-    //         puts("Raw:\t\t");
-    //         put_buffer(raw_buffer);
-    //         // process_buffer(buffer, raw_buffer);
-    //         // puts("Processed:\t");
-    //         // put_buffer(buffer);
-
-    //         putch('\n');
-
-    //         memset(raw_buffer, '\0', 1024);
-
-    //         puts("Type something here: ");
-    //         i=0;
-    //     } else if(raw_buffer[i] == '\b') {
-    //         raw_buffer[i-1] = '\0';
-    //         raw_buffer[i] = '\0';
-    //         i = i-1;
-    //     } else {
-    //         i++;
-    //     }
-    // }
-
-    do {
-        puts("Type something here: ");
+    while(1) {
+        puts("Enter a command: ");
         getline(buffer, 1024);
-        puts(buffer);
-        putch('\n');
-    } while(strcmp(buffer, "exit"));
+
+        if(!strcmp(buffer, "exit")) {
+            puts("OS shutting down.");
+            break;
+        } else if(!strcmp(buffer, "hello")) {
+            puts("Hi! How are you?\n");
+        } else if(!strcmp(buffer, "")) {
+            // puts("Please enter a command\n");
+
+            /* Do nothing if no command is entered */
+        } 
+        else {
+            putch('\'');
+            puts(buffer);
+            puts("\' is an unrecognized command\n");
+        }
+    }
+
+    // do {
+    //     puts("Type something here: ");
+    //     getline(buffer, 1024);
+    //     puts(buffer);
+    //     putch('\n');
+    // } while(strcmp(buffer, "exit"));
 
     puts("\nDone!");
 
